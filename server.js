4cfa33ce881ca8f22c18c9facbdff9363dc6f0ef
dair68 <granthuang68@yahoo.com>
2019-02-21 21:14:26 -0800

    set up login schema

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require("express");
-const path = require("path");
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//const Note = require("./noteModel.js");
 
 //middleware
 // app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Connecting to the Mongo DB
+mongoose.connect(
+    "mongodb://localhost/userdb",
+    { useNewUrlParser: true }
+    )
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));;
 
 app.get("/", (req, res) => res.send("Hello World"));
 
